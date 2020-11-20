f106b26704e61bd94cea5f354eea2e8b976219e3
freyjabun <freyjabun@gmail.com>
2020-11-20 12:40:39 +0100

    added java skeleton

---
+++ src/Genre.java
@@ -0,0 +1,7 @@
+package src;
+import java.util.List;
+
+public class Genre {
+    private List<Movie> movies;
+    private List<Series> series;
+}
---
+++ src/Movie.java
@@ -0,0 +1,5 @@
+package src;
+
+public class Movie extends Media {
+    
+}
