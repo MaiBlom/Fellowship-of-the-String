66e0f79ffb6f1f4c42372f6126493d5ffe7d003d
NaddiNadja <nadjabrixkoch@hotmail.com>
2020-11-20 13:14:42 +0100

    Added basic fields and methods for Genre.java

--- src/Genre.java
+++ src/Genre.java
@@ -1,7 +1,34 @@
 package src;
-import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Genre {
-    private List<Movie> movies;
-    private List<Series> series;
+    protected Set<Media> media;
+
+    public Genre() {
+        media = new HashSet<>();
+    }
+
+    // getters
+    public Set<Media> getMedia() {
+        return media;
+    }
+    public Set<Media> getMovies() {
+        Set<Media> movies = new HashSet<>();
+        for (Media m : media) {
+            if (m instanceof Movie) movies.add(m);
+        }
+        return movies;
+    }
+    public Set<Media> getSeries() {
+        Set<Media> series = new HashSet<>();
+        for (Media m : media) {
+            if (m instanceof Series) series.add(m);
+        }
+        return series;
+    }
+
+    public void add(Media m) {
+        media.add(m);
+    }
 }
