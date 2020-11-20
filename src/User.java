09d8c0150cc5c8be7b2bb16cc9e5eb1e546c13f4
NaddiNadja <nadjabrixkoch@hotmail.com>
2020-11-20 13:01:49 +0100

    Added basic fields and methods for User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,26 @@
 package src;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
+    protected String username;
+    protected Set<Media> favorites;
     
+    public User (String username) {
+        this.username = username;
+        this.favorites = new HashSet<>();
+    }
+
+    // getters
+    public String getUsername() {
+        return username;
+    }
+    public Set<Media> getFavorites() {
+        return favorites;
+    }
+
+    public void favorite(Media m) {
+        favorites.add(m);
+    }
 }
