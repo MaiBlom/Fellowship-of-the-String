91d4c3f14cd0e2e7c6a6eefa94abe64d02ea0552
NaddiNadja <nadjabrixkoch@hotmail.com>
2020-11-20 13:33:01 +0100

    Added basic fields and methods to MediaDB.java

--- src/MediaDB.java
+++ src/MediaDB.java
@@ -1,5 +1,19 @@
 package src;
 
+import java.util.Hashtable;
+
 public class MediaDB {
-    
+    Hashtable<String, Media> allMedia;
+
+    public MediaDB() {
+        allMedia = new Hashtable<>();
+    }
+
+    public void add(Media m) {
+        allMedia.put(m.getTitle(), m);
+    }
+
+    public Media get(String title) {
+        return allMedia.get(title);
+    }
 }
