1793c1704fd38b045ffff7711d4b280c002185a6
NaddiNadja <nadjabrixkoch@hotmail.com>
2020-12-01 13:25:24 +0100

    Added junit and first testclass

---
+++ tests/MediaDBtest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import src.*;
+
+public class MediaDBtest {
+    private MediaDB db;
+    @Before
+    public void setUp() {
+        this.db = MediaDB.getInstance();
+        FileReader fr = FileReader.getInstance(db);
+        fr.readAll();
+    }
+    
+    @Test
+    public void dbSize_equals200() {
+        //Arrange
+        int expected = 200;
+        
+        //Act
+        int actual = db.getAllMedia().size();
+        
+        //Assert
+        assertEquals(expected, actual);
+    }
+    
+    @After
+    public void tearDown() {
+        db = null;
+    }
+}
