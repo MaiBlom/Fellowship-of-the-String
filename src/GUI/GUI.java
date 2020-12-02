65506328a2c9b49a038e6f92fddf487332a0785b
maibl <mai.blommen@gmail.com>
2020-12-02 19:08:51 +0100

    Attempt 1 at making GUI

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -14,6 +14,9 @@ public class GUI extends JFrame {
     private Container contentPane;
     private Container nContainer;
     private Container cContainer;
+    private Container redakAnbContainer;
+    private Container movieContainer;
+    private Container seriesContainer;
 
     private final int WIDTH = 600, HEIGHT = 600;
     
@@ -32,6 +35,7 @@ public class GUI extends JFrame {
         contentPane = frame.getContentPane();
         contentPane.setLayout(new GridLayout(2, 1));
 
+        // Objects for top menu
         JButton favoritesbButton = new JButton("Favorites");
         JButton searchButton = new JButton("Search");
         JButton userButton = new JButton("User");
@@ -39,18 +43,57 @@ public class GUI extends JFrame {
         nContainer = new Container();
         nContainer.setLayout(new GridLayout(1, 3));
 
+        // Add objects for top menu
         nContainer.add(favoritesbButton, BorderLayout.EAST);
         nContainer.add(searchButton, BorderLayout.WEST);
         nContainer.add(userButton, BorderLayout.WEST);
 
         contentPane.add(nContainer, BorderLayout.NORTH);
 
+        // Middle container
         cContainer = new Container();
-        cContainer.setLayout(new BorderLayout(3, 1));
+        cContainer.setLayout(new GridLayout(3, 1));
 
-        // Mai gave up right here and will continue to do stuff later tonight
+        // Middle container recommended by "redaktionen"
+        redakAnbContainer = new Container();
+        redakAnbContainer.setLayout(new GridLayout(2,1));
+        
+        JLabel redakLabel = new JLabel("Redaktionen anbefaler:");
+        JLabel mediaTestLabel = new JLabel("Recommendations here");
+        
+        redakAnbContainer.add(redakLabel);
+        redakAnbContainer.add(mediaTestLabel);
 
-        frame.setSize(new Dimension(WIDTH, HEIGHT));
+        // Middle container movie list
+        movieContainer = new Container();
+        movieContainer.setLayout(new GridLayout(2,1));
+
+        JLabel movieLabel = new JLabel("Movies:");
+        JLabel movieTestLabel = new JLabel("Movies here");
+        
+        movieContainer.add(movieLabel);
+        movieContainer.add(movieTestLabel);
+
+        // Middle container series list
+        seriesContainer = new Container();
+        seriesContainer.setLayout(new GridLayout(2,1));
+
+        JLabel seriesLabel = new JLabel("Series:");
+        JLabel seriesTestLabel = new JLabel("Series here");
+        
+        seriesContainer.add(seriesLabel);
+        seriesContainer.add(seriesTestLabel);
+
+        // Add the 3 media containers to the center container
+        cContainer.add(redakAnbContainer);
+        cContainer.add(movieContainer);
+        cContainer.add(seriesContainer);
+
+        // Add the center container to the contentPane
+        contentPane.add(cContainer);
+
+        //frame.setSize(new Dimension(WIDTH, HEIGHT));
+        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
